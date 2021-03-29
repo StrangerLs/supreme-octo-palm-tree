1d5f6eb15eb71cd221beede1ff9529c61b9c5483
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-03-29 13:31:49 -0400

    created react-app

---
+++ client/src/data.js
@@ -0,0 +1,13 @@
+const posts = [
+  {
+    "name": "Dylan",
+    "imgURL": "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb.jpg",
+    "content": "Yay dogs!"
+  },
+  {
+    "name": "Molly",
+    "imgURL": "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9",
+    "content": "Yay cats!"
+  }
+
+]
