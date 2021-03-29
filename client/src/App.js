6dda8d4d6ab377db5ae10d5dfdd589ba4567f5a9
mollyexten <molly.exten@gmail.com>
2021-03-29 14:52:18 -0400

    Set up routes in app.js and deleted posts screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CreatePost from "./screens/CreatePost/CreatePost";
+import EditPost from "./screens/EditPost/EditPost";
+import Home from "./screens/Home/Home";
+import PostDetail from "./screens/PostDetail/PostDetail";
 
 function App() {
   return (
     <div className="App">
-<h2>hey</h2>
+      <h2>hey</h2>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/add-post" component={CreatePost} />
+        <Route exact path="/posts/:id/edit" component={EditPost} />
+        <Route exact path="/products/:id" component={PostDetail} />
+      </Switch>
     </div>
   );
 }
