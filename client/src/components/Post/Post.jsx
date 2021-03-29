f2446b0c0238797198e117bdf4d08e11cee0f3a9
mollyexten <molly.exten@gmail.com>
2021-03-29 15:45:32 -0400

    Changed html tags in post component to be more semantic, added class names to the different tags in the post component

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -3,11 +3,11 @@ import "./Post.css";
 function Post(props) {
   const post = props.post;
   return (
-    <div className="post-item">
-      <p>{post.name}</p>
-      <img src={post.imgURL} alt="post-image" width="300px"/>
-      <p>{post.content}</p>
-    </div>
+    <article className="blog-post">
+      <h2 className="post-name">{post.name}</h2>
+      <img src={post.imgURL} alt="post-image" width="300px" className="post-image"/>
+      <p className="post-content">{post.content}</p>
+    </article>
   )
 }
 
