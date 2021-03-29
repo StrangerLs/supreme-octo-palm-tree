1317e9f4d5bcd363926ab566aa0a82c0b0891e3e
mollyexten <molly.exten@gmail.com>
2021-03-29 16:01:49 -0400

    created template for all screen components, working on the postdetail screen

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Post.css";
 
 function Post(props) {
   const post = props.post;
   return (
     <article className="blog-post">
-      <h2 className="post-name">{post.name}</h2>
-      <img src={post.imgURL} alt="post-image" width="300px" className="post-image"/>
+      <Link to={`/posts/${post.id}`}>
+        <h2 className="post-title">{post.title}</h2>
+      </Link>
+      <h3 className="post-name">{post.name}</h3>
+      <img
+        src={post.imgURL}
+        alt="post-image"
+        width="300px"
+        className="post-image"
+      />
       <p className="post-content">{post.content}</p>
     </article>
-  )
+  );
 }
 
 export default Post;
---
+++ client/src/screens/CreatePost/CreatePost.jsx
@@ -0,0 +1,7 @@
+function CreatePost(props) {
+  return (
+    <div></div>
+  )
+}
+
+export default CreatePost;
