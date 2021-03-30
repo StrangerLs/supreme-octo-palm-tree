10d36f70467a01a1ce367c02e5795ea3e89d4f8e
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-03-30 15:18:51 -0400

    rendered post details

--- client/src/screens/PostDetail/PostDetail.jsx
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -1,29 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../../components/shared/Layout/Layout";
+import { getContent, deleteContent } from "../../services/contents"
 
 function PostDetail(props) {
-  const [post, setPost] = useState(null);
+  const [posts, setPosts] = useState(null);
   const [isLoaded, setLoaded] = useState(false)
   const { id } = useParams()
-  const posts = props.posts
+  
+  
   useEffect(() => {
-    const foundProduct = posts.find(post => post.id === id)
-    setPost(foundProduct);
-    setLoaded(true)
+
+    const getPost = async () => {
+      const post = await getContent(id)
+      setPosts(post)
+      setLoaded(true)
+    }
+
+    getPost()
   }, [id])
 
   if (!isLoaded) {
     return <h1>NO POST FOUND</h1>
   }
-
   return (
     <div>
       <Layout>
-        <h2>{post.title}</h2>
-        <h3>{post.author}</h3>
-        <img src={post.imgURL} width="300px" />
-        <p>{post.post}</p>
+        <h2>{posts.title}</h2>
+        <h3>{posts.author}</h3>
+        <img src={posts.imgURL} width="300px" />
+        <p>{posts.post}</p>
       </Layout>
     </div>
   )
