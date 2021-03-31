66c8e3b259b2464d2b4e259e19145cb98ce4c4a4
mollyexten <molly.exten@gmail.com>
2021-03-31 13:05:55 -0400

    added comments to the form screen

--- client/src/screens/Form/Form.jsx
+++ client/src/screens/Form/Form.jsx
@@ -5,50 +5,59 @@ import { createContent, getContent, updateContent } from "../../services/content
 import { useParams, Redirect } from 'react-router-dom'
 
 function Form() {
-  const [created, setCreated] = useState("")
-
-  const [newPost, setNewPost] = useState({
+  // isCreated and isUpdated will tell the handleSubmit whither the user shall be directed
+  const [isCreated, setCreated] = useState("")
+  const [isUpdated, setUpdated] = useState(false);
+  
+  // post will store the form input values
+  const [post, setPost] = useState({
     title: "",
     author: "",
     imgURL: "",
     post: "",
   });
 
-  const [isUpdated, setUpdated] = useState(false);
+  // id will capture the id value from params in the case of editing a post
   let { id } = useParams();
-  console.log(id)
 
+  // Aforementioned id will trigger the component to get information about the post and fill out the form with this data
   useEffect(() => {
     const fetchContent = async () => {
       const content = await getContent(id);
-      setNewPost(content);
+      setPost(content);
     };
     fetchContent();
   }, [id]);
 
+  // Whenever the user types within the form inputs, handleChange will trigger and update the state of post
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setNewPost({
-      ...newPost,
+    setPost({
+      ...post,
       [name]: value,
     });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+    // If there is an id, handleSubmit will update the existing post and change the state of isUpdated
     if (id) {
-      const updated = await updateContent(id, newPost);
+      const updated = await updateContent(id, post);
     setUpdated(updated);
-    } else {
-      const created = await createContent(newPost)
+    }
+    // Otherwise, handleSubmit will create a new post to the api and update the value of isCreated
+    else {
+      const created = await createContent(post)
       setCreated({ created })
     }
   }
 
-  if (created) {
+  // Redirect to Home if the new post was created
+  if (isCreated) {
     return <Redirect to={`/`} />
   }
   
+  // Redirect to postDetail screen if a post was updated
   if (isUpdated) {
     return <Redirect to={`/posts/${id}`} />
   }
@@ -58,10 +67,10 @@ function Form() {
       <Layout>
         <h3>Create Post Here</h3>
         <form onSubmit={handleSubmit} className="form-container">
-          <input type="text" name="title" value={newPost.title} onChange={handleChange} placeholder="title" className="title-input"/>
-          <input type="text" name="author" value={newPost.author} onChange={handleChange} placeholder="author" className="author-input"/>
-          <input type="text" name="imgURL" value={newPost.imgURL} onChange={handleChange} placeholder="image URL" className="img-input"/>
-          <textarea name="post" value={newPost.post} onChange={handleChange} placeholder="write your thoughts here..." rows="6" className="post-input"/>
+          <input type="text" name="title" value={post.title} onChange={handleChange} placeholder="title" className="title-input"/>
+          <input type="text" name="author" value={post.author} onChange={handleChange} placeholder="author" className="author-input"/>
+          <input type="text" name="imgURL" value={post.imgURL} onChange={handleChange} placeholder="image URL" className="img-input"/>
+          <textarea name="post" value={post.post} onChange={handleChange} placeholder="write your thoughts here..." rows="6" className="post-input"/>
           <button type="submit" className="submit-button">submit</button>
         </form>
       </Layout>
